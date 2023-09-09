9aaaf38a8fa85266c72b3887780d276daa4bb351
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-09-09 16:32:56 -0300

    fetch products

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
+import { Info } from "./components/Info";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("https://coding-challenge-api.aerolab.co/products", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <>
       <Header />
       <main>
         <Hero />
+        <Info />
       </main>
     </>
   );
