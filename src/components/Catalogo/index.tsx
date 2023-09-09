e73f1847a673fb73c48798a1f60833909c54c106
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-09-09 16:31:46 -0300

    create component catalogo

---
+++ src/components/Catalogo/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "../Container";
+import { StyledCatalogo } from "./style";
+
+export const Catalogo = () => {
+  return (
+    <Container>
+      <StyledCatalogo>
+        <h2>
+          Tech <span>products</span>
+        </h2>
+      </StyledCatalogo>
+    </Container>
+  );
+};
